2b9fab22a763506bc7e869a2d2a8925470481fe7
Viktoria Kirina <kirinavi@outlook.com>
2023-08-15 03:19:59 +0300

    add filter method

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,71 +1,315 @@
-import React, { useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import data from '../../products.json'
 import './style.scss'
 import Card from './Card/Card';
 import Pagination from '../Pagination/Pagination';
-import {ProductsContext} from '../../context/ProductsContext'
-import { ACTIONS, reduser, initialState } from '../../redusers/reducer';
+// import {ProductsContext} from '../../context/ProductsContext'
+// import { ACTIONS, reduser, initialState } from '../../redusers/reducer';
 
-const PRODUCTS_PER_PAGE = 12
+const PRODUCTS_PER_PAGE = 9
+
+const ACTIONS = {
+  // SET_PRODUCTS: 'set_products',
+  SORT_PRODUCTS: 'sort_products',
+  FILTER_PRODUCTS: 'filter_products',
+  LIKE_PRODUCTS: 'like_products',
+  ADD_PRODUCTS: 'add_products',
+}
+
+const SORTING = {
+  RELEVANCE: 'relevance',
+  CHEAP: 'cheap',
+  EXPENSIVE: 'expensive',
+  NEW: 'new',
+  DISCOUNT: 'discount'
+}
+
+const CATEGORIES = {
+  ALL: 'All',
+  MEN: 'Men',
+  WOMEN: 'Women',
+  ACC: 'Accessories',
+  NEW: 'New Arrivals'
+}
+
+// const sortOptions = [SORTING.RELEVANCE, SORTING.CHEAP, SORTING.EXPENSIVE, SORTING.NEW, SORTING.DISCOUNT]
 
 export default function Products() {
+  const [products, setProducts] = useState(data.products)
+  const [sortMethod, setSortMethod] = useState(SORTING.RELEVANCE)
+  const [filterMethod, setFilterMethod] = useState(null)
+  const [filterAll, setFilterAll] = useState(0)
+  const [filterMen, setFilterMen] = useState(0)
+  const [filterWomen, setFilterWomen] = useState(0)
+  const [filterAcc, setFilterAcc] = useState(0)
+  const [filterNew, setFilterNew] = useState(0)
+
+  const [checked, setChecked] = useState(0)
+
   const [curPage, setCurPage] = useState(1)
-  // const [likedCards, setLikedCards] = useState(null)
+  // const [likedCards, setLikedCards] = useState([])
   //
-  const [state, dispatch] = useReducer(reduser, initialState)
+  // const [state, dispatch] = useReducer(reduser, initialState)
   //
+  // const [process, setProcess] = useState('')
+  //!!запомнить в массив избранные товары
 
   const firstIndex = (curPage - 1) * PRODUCTS_PER_PAGE
   const lastIndex = firstIndex + PRODUCTS_PER_PAGE
-  const totalPages = data.products.length / PRODUCTS_PER_PAGE;
+  const totalPages = products.length / PRODUCTS_PER_PAGE;
+  let slisedCards = products.slice(firstIndex, lastIndex)
+
+  //рабочая сортировка по всем карточкам
+  const sorting = (sortMethod) => {
+    if (sortMethod) {
+      switch (sortMethod) {
+        case SORTING.RELEVANCE:
+          setProducts(data.products.sort((prod1, prod2) => prod1.id > prod2.id ? 1 : -1))
+          break
+
+        case SORTING.CHEAP:
+          setProducts(data.products.sort((prod1, prod2) => prod1.price > prod2.price ? 1 : -1))
+          break
+              
+        case SORTING.EXPENSIVE:
+          setProducts(data.products.sort((prod1, prod2) => prod1.price < prod2.price ? 1 : -1))
+          break
+    
+        case SORTING.NEW:
+          setProducts(data.products.sort((prod1, prod2) => prod1.isNew < prod2.isNew ? 1 : -1))
+          break
+    
+        case SORTING.DISCOUNT:
+          setProducts(data.products.sort((prod1, prod2) => prod1.isSale < prod2.isSale ? 1 : -1))
+          break
+    
+        default:
+          setProducts(data.products)
+      }
+    }
+             
+    return slisedCards = products.slice(firstIndex, lastIndex)
+  }
+
+  //рабочий фильтр через селект
+  const filtering = (filterMethod) => {
+    let filtered = []
+
+    switch (filterMethod) {
+      case CATEGORIES.ALL:    
+        filtered = data.products
+        break
+  
+      case CATEGORIES.MEN:
+        filtered = products.filter(prod => prod.categories.includes('Men'))
+        console.log(filtered)
+        break
+  
+      case CATEGORIES.WOMEN:
+        filtered = products.filter(prod => prod.categories.includes('Women'))
+        break
+  
+      case CATEGORIES.ACC:
+        filtered = products.filter(prod => prod.categories.includes('Accessories'))
+        break
+  
+      case CATEGORIES.NEW:
+        filtered = products.filter(prod => prod.isNew == 1)
+        break
+
+      default:
+        filtered = data.products
+    }
+           
+    slisedCards = filtered.slice(firstIndex, lastIndex)
+    return filtered
+  }
 
   //!! Добавить функцию обработки фильтра и сортировки
-  const slicedCards = data.products.slice(firstIndex, lastIndex)
+  const filterCards = (filterMethod) => { 
+    let filtered = products 
+
+    // if (filterAll && filterMethod == CATEGORIES.ALL) {
+    //   filtered = data.products
+    // }
+
+    // if (filterAcc) {
+    //   filtered = filtered.filter(prod => prod.categories.includes('Accessories'))
+    // }
+
+    // if (filterWomen) {
+    //   filtered = filtered.filter(prod => prod.categories.includes('Women'))
+    // }
+    // else if (filterWomen && filterMethod == CATEGORIES.MEN) {
+    //   filtered = products.filter(prod => prod.categories.includes('Men'))
+    // }
+    // else if (filterWomen && filterMethod == CATEGORIES.NEW) {
+    //   filtered = products.filter(prod => prod.isNew == 1)
+    // }
+    // else filtered = data.products
+    // console.log(filtered)
+
+    switch (filterMethod) {
+      case CATEGORIES.ALL:    
+        filtered = products
+        break
+  
+      case CATEGORIES.MEN:
+        filtered = products.filter(prod => prod.categories.includes('Men'))
+        console.log(filtered)
+        break
+  
+      case CATEGORIES.WOMEN:
+        filtered = products.filter(prod => prod.categories.includes('Women'))
+        break
+  
+      case CATEGORIES.ACC:
+        filtered = products.filter(prod => prod.categories.includes('Accessories'))
+        break
+  
+      case CATEGORIES.NEW:
+        filtered = products.filter(prod => prod.isNew == 1)
+        break
+
+      default:
+        filtered = data.products
+    }
+
+            
+    slisedCards = filtered.slice(firstIndex, lastIndex)
+    // setProducts(filtered)
+    return filtered
+  }
+
+  const check = () => {
+    let arr = data.products[2].categories
+    // for (let i = 0; i < arr.length; i++) {
+    //   // console.log(arr[i])
+      
+    //   if (arr[i] === 'Women') console.log('success', arr[i])
+    // }
+    // console.log(arr.length)
+    // console.log('arr = ', arr)
+
+
+    arr.forEach(elem => elem === 'Men' ? console.log('success', elem) : console.log('fail', elem) )
+
+    const answer = data.products.filter((prod) => prod.categories.forEach(cat => cat === 'Men' ? true : false ))
+
+    if (answer) {
+      console.log('MEN exsists')
+    }
+  }
+
+  const log = (sorted) => {
+    sorted.forEach(sort => console.log('sorted:', sort))
+  }
+
+  // useEffect(() => {
+  //   setProducts(filterCards())
+  // }, [filterMethod])
+
 
   const changeCurPage = (page) => {
     setCurPage(page)
   }
 
-  //!!запомнить в массив избранные товары
-  const setLikedCards = () => {
+  const changeFilterMethod = (e) => {
+    setFilterMethod(e.target.value)
+    // console.log(filterMethod)
+    // e.target.value ? setFilterMethod(e.target.value) : setFilterMethod(filterMethod)
     
   }
 
   return(
-    <ProductsContext.Provider value={{ dispatch, state }}> 
       <section className='wrapper blocks'>
         <section className='products'>
           <div className='header'>
             <div className='amount'>
-              <p>There are <span>{data.products.length}</span> products in this category</p>
+              <p>There are <span>{products.length}</span> products in this category</p>
+              <p>FILTER METHOD: {filterMethod}</p>
             </div>
 
             <div className='sort'>
-              <select defaultValue={'relevance'} className='select' 
-                onChange={(e) => 
-                  dispatch({
-                  type: ACTIONS.SORT_PRODUCTS,
-                  payload: e.target.value
-              })}>
-                <option value='relevance'>By relevance</option>
-                <option value='cheap'>From cheap to expensive</option>
-                <option value='expensive'>From expensive to cheap</option>
-                <option value='new'>New products</option>
-                <option value='discount'>Discount goods</option>
+              <select value={sortMethod} className='select' onChange={e => setSortMethod(e.target.value)}>
+                <option value={SORTING.RELEVANCE}>By relevance</option>
+                <option value={SORTING.CHEAP}>From cheap to expensive</option>
+                <option value={SORTING.EXPENSIVE}>From expensive to cheap</option>
+                <option value={SORTING.NEW}>New products</option>
+                <option value={SORTING.DISCOUNT}>Discount goods</option>
               </select>
             </div>
+
+            {/* <div className='sort'> */}
+              {/* <input type='checkbox' name='sortAll' value={filterMethod} className='select' onChange={e => setFilterMethod(e.target.value)}> */}
+                <label> 
+                  <input type='checkbox' name='sortAll' value={CATEGORIES.ALL} checked={filterAll} 
+                    onChange={() => {
+                      setFilterAll(!filterAll)
+                      changeFilterMethod(CATEGORIES.ALL)
+                    }} 
+                  />
+                  {CATEGORIES.ALL}
+                </label>
+                <label> 
+                  <input type='checkbox' name='sortMen' value={CATEGORIES.MEN} checked={filterMen} 
+                    onChange={(e) => {
+                      setFilterMen(!filterMen)
+                      // changeFilterMethod()
+                      filterCards()
+                    }} 
+                  />
+                  {CATEGORIES.MEN}
+                </label>
+                <label> 
+                  <input type='checkbox' name='sortWomen' value={CATEGORIES.WOMEN} checked={filterWomen} 
+                    onChange={(e) => {
+                      setFilterWomen(!filterWomen)
+                      // changeFilterMethod(e)
+                      
+                      // changeFilterMethod(e.target.value)
+                      // checked ? changeFilterMethod(e.target.value) : changeFilterMethod(null)
+                    }}  
+                  />
+                  {CATEGORIES.WOMEN}
+                </label>
+                <label> 
+                  <input type='checkbox' name='sortAcc' value={CATEGORIES.ACC} checked={filterAcc} 
+                    onChange={(e) => {
+                      setFilterAcc(!filterAcc)
+                      // changeFilterMethod(e)
+                      // checked ? changeFilterMethod(e.target.value) : changeFilterMethod(null)
+                    }}  
+                  />
+                  {CATEGORIES.ACC}
+                </label>
+                <label> 
+                  <input type='checkbox' name='sortNew' value={CATEGORIES.NEW} checked={filterNew} 
+                  onChange={(e) => {
+                    setFilterNew(!filterNew)
+                    // changeFilterMethod(e)
+                  }} 
+                  />
+                  {CATEGORIES.NEW}
+                </label>
+
+                {/* <option value={CATEGORIES.MEN}>Men</option>
+                <option value={CATEGORIES.WOMEN}>Women</option>
+                <option value={CATEGORIES.ACC}>Accessories</option>
+                <option value={CATEGORIES.NEW}>New Arrivals</option> */}
+              {/* </input> */}
+            {/* </div> */}
           </div>
 
           <div className='cards'>
             {
-              data.products.length && slicedCards.map((product) => <Card key={product.id} product={product} />)
+              products.length && slisedCards.map((product) => <Card key={product.id} product={product} /> )
             }
           </div>
 
-          <Pagination totalPages={totalPages} changeCurPage={changeCurPage} curPage={curPage}/>
+          <Pagination totalPages={totalPages} changeCurPage={changeCurPage} curPage={curPage} />
         </section>
       </section>   
-    </ProductsContext.Provider>
   )
 
 }
--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -7,26 +7,19 @@ export default function Content() {
       <div className='inner-content'>
         <div className='right-block'>
           <section className='dots'/>
-
           <section className='content-block'>
             <section className='inner-text'>
               <h1>Shop</h1>
-
               <section>
                 <span className='maintext blacktext'>Home</span>
                 <span className='maintext redtext'>Shop</span>
               </section>
             </section>
-
           </section>
-
-
-          {/* <section className='line'></section> */}
         </div>
 
         <div className='left-block'>
           <img src="/img/bg.png" alt="bg" />
-
         </div>
       </div>
   </div>
