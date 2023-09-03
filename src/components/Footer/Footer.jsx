dc3f149eb63c37a3ec8b992c2f3b103883244285
Viktoria Kirina <kirinavi@outlook.com>
2023-09-03 23:07:32 +0300

    add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import { ReactComponent as FooteLeft } from '../../img/footer-left.svg';
+import { ReactComponent as FooteRight } from '../../img/footer-right.svg';
+
+
+export default function Footer() {
+  
+
+  return (
+    <div className="wrapper">
+      <div className="image-left">
+
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div className="image-right"></div>
+    </div>
+
+  )
+}
