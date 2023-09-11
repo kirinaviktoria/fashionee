183d112bdef094f9549b77188567112b813b4fa3
Vicktoria Kirina <kirinavi@outlook.com>
2023-09-11 22:34:34 +0300

    clear reducer in main branch

--- src/redusers/reducer.js
+++ src/redusers/reducer.js
@@ -1,9 +1,6 @@
 import data from '../products.json'
 
 export const ACTIONS = {
-  // SET_PRODUCTS: 'set_products',
-  SORT_PRODUCTS: 'sort_products',
-  FILTER_PRODUCTS: 'filter_products',
   LIKE_PRODUCTS: 'like_products',
   ADD_PRODUCTS: 'add_products',
 }
@@ -11,47 +8,8 @@ export const ACTIONS = {
 export const initialState = {
   products: []
 }
-
-const sort = (method) => {
-  switch (method) {
-    case 'relevance':
-      return {
-        sorted: data.products.sort((prod1, prod2) => prod1.id > prod2.id ? 1 : -1)
-      };
-
-    case 'cheap':
-      return {
-        sorted: data.products.sort((prod1, prod2) => prod1.price > prod2.price ? 1 : -1)
-      };
-
-    case 'expensive':
-      return {
-        sorted: data.products.sort((prod1, prod2) => prod1.price < prod2.price ? 1 : -1)
-      };
-
-    case 'new':
-      return {
-        sorted: data.products.sort((prod1, prod2) => prod1.isNew < prod2.isNew ? 1 : -1)
-      };
-
-    case 'discount':
-      return {
-        sorted: data.products.sort((prod1, prod2) => prod1.isSale < prod2.isSale ? 1 : -1)
-      };
-
-    default:
-      return data.products
-  }
-}
-
 export const reduser = (state, action) => {
   switch (action.type) {
-    case ACTIONS.SORT_PRODUCTS:
-      return {
-        ...state,
-        sorted: sort(action.payload)
-      }
-
     //разобраться, как добавить карточки в локалсторедж
     case ACTIONS.LIKE_PRODUCTS:
       return {
